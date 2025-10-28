6004772614890c75c3a72169e480ea5d039acc92
wata548 <watashimokia@gmail.com>
2025-10-28 18:57:38 +0900

    Player movement, but not good, I didn't make camera pitch limit, but it worked.

---
+++ mincraft/Assets/Scripts/Extension/ExSingle.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine.Timeline;
+
+namespace Extension {
+    public static class ExSingle {
+        public static float Sign(this float pValue) =>
+            pValue == 0 ? 0f : pValue > 0 ? 1f : -1f;
+    }
+}
--- mincraft/Assets/Scripts/Player/Movement.cs
+++ mincraft/Assets/Scripts/Player/Movement.cs
@@ -1,67 +1,110 @@
-﻿using System;
-using System.Linq;
-using Extension;
+﻿using Extension;
 using UnityEngine;
 
 namespace MapGenerator.Player {
     [RequireComponent(typeof(Rigidbody))]
-    public class Movement: MonoEasyBehaviour {
+    public class Movement: MonoBehaviour {
 
-        //peekTime = 0.2, peekHeight = 1.7 => gravityScale = -75, jumpScale = 15
-        private const float JUMP_SCALE = 11.333f;
+        //peekTime = 0.3 = 1.3 => gravityScale = -28.8888..., jumpScale = 8.666...
+        private const float JUMP_SCALE = 8.66f;
+        private const float CONTACT_RANGE = 0.05f;
+        private const float SPEED = 6f;
 
-        private int _contactCount = 0;
         private bool _isGround;
+        private Camera _camera;
+        private Rigidbody _rigidbody;
+#if UNITY_EDITOR
+        private Vector3 E_inputDirection = Vector3.forward;
+        private bool E_isStop = false;
+#endif
+        
+        private void Awake() {
+            _rigidbody = GetComponent<Rigidbody>();
+            _camera = Camera.main!;
+        }
 
         private void Update() {
-
-            var direction = InputSetting.GetDirection();
-            var delta = 6 * direction;
-            var velocity = Component<Rigidbody>().velocity;
             
-            var pos = transform.position + 0.5f * direction.Multiple(transform.localScale);
-            var scale = transform.localScale * 0.49f;
-            var rotation = Quaternion.Euler(0, Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg, 0);
-            scale.z = 0.05f;
-            if (Physics.OverlapBox(pos, scale, rotation, LayerMask.GetMask("Ground")).Length > 0) {
-                delta = Vector3.zero;
-            }
-            
+            CameraUpdate();
+            var delta = InputDelta();
+            var velocity = _rigidbody.velocity;
             (velocity.x, velocity.z) = (delta.x, delta.z);
             
             if (_isGround && Input.GetKeyDown(InputSetting.Jump))
                 velocity.y += JUMP_SCALE;
 
-            Component<Rigidbody>().velocity = velocity;
-            pos = transform.position;
-            pos.y -= transform.localScale.y / 2;
-            scale = transform.localScale * 0.49f;
-            scale.y = 0.05f;
-            var groundCount = Physics.OverlapBox(pos, scale, Quaternion.identity, LayerMask.GetMask("Ground")).Length;
+            _rigidbody.velocity = velocity;
+            GroundCheck();
+        }
+
+        private void GroundCheck() {
+            var pos = transform.position;
+            pos.y -= transform.localScale.y * 0.5f;
+            var scale = transform.localScale * 0.49f;
+            scale.y = CONTACT_RANGE;
+            var groundCount = Physics.OverlapBox(pos, scale, transform.rotation, LayerMask.GetMask("Ground")).Length;
             _isGround = groundCount != 0;
-
         }
+        
+        private Vector3 InputDelta() {
+            var rotation = transform.rotation;
+            var direction = InputSetting.GetDirection();
 
-        private void OnCollisionEnter(Collision other) {
-            _contactCount++;
+            
+            var delta = SPEED * (rotation * direction);
+            
+            var pos = transform.position + 0.5f * (rotation * direction.Multiple(transform.localScale));
+            var scale = transform.localScale * 0.49f;
+            scale.z = CONTACT_RANGE;
+            
+            var directionRotation = Quaternion.Euler(0, Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg, 0);
+            if (Physics.OverlapBox(pos, scale, directionRotation * rotation, LayerMask.GetMask("Ground")).Length > 0) {
+                delta = Vector3.zero;
+            }
+#if UNITY_EDITOR
+            E_inputDirection = direction;
+            E_isStop = delta == Vector3.zero;
+#endif
+
+            return delta;
         }
+        
+        private void CameraUpdate() {
 
-        private void OnCollisionExit(Collision other) {
-            _contactCount--;
+            var mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), -Input.GetAxisRaw("Mouse Y"));
+            var rotation = transform.rotation.eulerAngles;
+            rotation.y += mouseDelta.x;
+            
+            var pitch = _camera.transform.rotation.eulerAngles.x + mouseDelta.y;
+            _camera.transform.localRotation = Quaternion.Euler(pitch, 0, 0);
+            transform.rotation = Quaternion.Euler(rotation);
         }
 
         private void OnDrawGizmos() {
             
-            var pos = transform.position;
-            pos.y -= transform.localScale.y / 2 + 0.06f;
-            var scale = transform.localScale * 0.49f;
-            scale.y = 0.05f;
-            scale *= 2;
+            Gizmos.matrix = Matrix4x4.TRS(transform.position, transform.rotation, transform.lossyScale);
+            var pos = Vector3.down * 0.5f;
+            var scale = Vector3.one * 0.9f;
+            scale.y = transform.InverseTransformVector(Vector3.up * CONTACT_RANGE * 2).magnitude;
+            
             Gizmos.color = Color.red;
-            Gizmos.DrawCube(pos, scale);
+            Gizmos.DrawWireCube(pos, scale);
             
+#if UNITY_EDITOR
+            var rotation = transform.rotation;
+            pos = transform.position + 0.5f * ( rotation * E_inputDirection.Multiple(transform.localScale));
+            scale = transform.localScale * 0.49f;
+            scale.z = CONTACT_RANGE;
             
+            var directionRotation = Quaternion.Euler(0,
+                Mathf.Atan2(E_inputDirection.x, E_inputDirection.z) * Mathf.Rad2Deg,
+                0);
             
+            Gizmos.color = E_isStop ? Color.blue : Color.green;
+            Gizmos.matrix = Matrix4x4.TRS(pos, directionRotation * transform.rotation, Vector3.one);
+
+            Gizmos.DrawWireCube(Vector3.zero, scale * 2f);
+#endif
         }
     }
 }
