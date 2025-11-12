1c3612a31c21dda0dcd14052b43774e34da5928f
wata548 <watashimokia@gmail.com>
2025-11-12 17:32:16 +0900

    Split interaction code

--- mincraft/Assets/Scripts/Entity/Player/CameraControl.cs
+++ mincraft/Assets/Scripts/Entity/Player/CameraControl.cs
@@ -1,18 +1,14 @@
-﻿using System;
-using Extension;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Player {
     public class CameraControl: MonoBehaviour {
 
-        private const float INTERACT_RANGE = 5;
         
         //==================================================||Fields 
-        [SerializeField] private GameObject _selectBox;
         
         [SerializeField] private Vector2 _initPos = Vector2.zero;
-        private Vector3 _point = Vector3.zero; 
         private Camera _camera = null;
+        public Transform CameraTransfom => _camera.transform;
         
         //==================================================||Methods 
         private void CameraUpdate() {
@@ -25,15 +21,7 @@ namespace Player {
             _camera.transform.localRotation = Quaternion.Euler(pitch, 0, 0);
             transform.rotation = Quaternion.Euler(rotation);
         }
-
-        public void Interact() {
-            
-            if (!Physics.Raycast(_camera.transform.position, _camera.transform.forward, out var hit, INTERACT_RANGE))
-                return;
-
-            _point = (hit.point + _camera.transform.forward * 0.01f).ToVec3Int() + Vector3.one * 0.5f;
-        }
-
+        
         //==================================================||Unity 
         private void Awake() {
             _camera = Camera.main!;
@@ -43,21 +31,6 @@ namespace Player {
 
         private void Update() {
             CameraUpdate();
-
-            if (Input.GetMouseButtonDown(0)) {
-                Interact();
-                _selectBox.SetActive(true);
-                _selectBox.transform.position = _point;
-            }
-
-            if (Input.GetMouseButton(0)) {
-                
-                _selectBox.transform.position = _point;
-                _selectBox.transform.rotation = Quaternion.identity;
-            }
-
-            if (Input.GetMouseButtonUp(0))
-                _selectBox.SetActive(false);
         }
     }
 }
---
+++ mincraft/Assets/Scripts/Entity/Player/Interaction.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Extension;
+using UnityEngine;
+
+namespace Player {
+    
+    [RequireComponent(typeof(CameraControl))]
+    public class Interaction: MonoBehaviour {
+        
+       //==================================================||Constant 
+        private const float INTERACT_RANGE = 5;
+        
+       //==================================================||Fields 
+        [SerializeField] private GameObject _selectBoxPrefab;
+        private CameraControl _cameraControl;
+        
+        private Vector3 _point = Vector3.zero;
+        private GameObject _selectBox;
+        
+       //==================================================||Methods 
+        public void Interact() {
+
+            var cameraTransform = _cameraControl.CameraTransfom;
+            if (!Physics.Raycast(cameraTransform.position, cameraTransform.forward, out var hit, INTERACT_RANGE))
+                return;
+
+            _point = (hit.point + cameraTransform.forward * 0.01f).ToVec3Int() + Vector3.one * 0.5f;
+        }
+    
+        //==================================================||Unity 
+        private void Awake() {
+            _selectBox = Instantiate(_selectBoxPrefab);
+        }
+
+        private void Update() {
+
+            if (Input.GetMouseButtonDown(0)) {
+                _selectBox.SetActive(true);
+            }
+
+            if (Input.GetMouseButton(0)) {
+                
+                Interact();
+                _selectBox.transform.position = _point;
+                _selectBox.transform.position = _point;
+                _selectBox.transform.rotation = Quaternion.identity;
+            }
+
+            if (Input.GetMouseButtonUp(0))
+                _selectBox.SetActive(false);
+        }
+    }
+}
